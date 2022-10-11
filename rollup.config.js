53d6b7a8cb161d0b273b4c3200fb0f52cd72470f
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-10-11 07:54:04 +0300

    add entityTag attribute (#5)
    * add entityTag attribute
    
    * 2.7.3

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import * as pkg from './package.json';
 import dts from 'rollup-plugin-dts';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default [
     {
@@ -48,6 +49,7 @@ export default [
             [ '@themost/cache' ]
         ),
         plugins: [
+            nodeResolve(),
             babel({
                 babelHelpers: 'bundled'
             }),
@@ -64,9 +66,12 @@ export default [
         external: Object.keys(pkg.dependencies).concat(Object.keys(pkg.peerDependencies)).concat(
             [ '@themost/cache' ]
         ),
-        plugins: [babel({
-            babelHelpers: 'bundled'
-        })]
+        plugins: [
+            nodeResolve(),
+            babel({
+                babelHelpers: 'bundled'
+            })
+        ]
     },
     {
         input: 'platform-server/src/index.d.ts',
